c4dcfe56ba876c636044b411697853ae73de0945
nagireddy1234 <nagireddy.panditi@gmail.com>
2021-10-15 12:33:43 +0530

    added the proxy to fix cors

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const PROXY = "https://cors-anywhere.herokuapp.com";
+
 export const searchAPIcall = async (query: string) => {
   try {
     const result = await axios(
-      `http://suggestqueries.google.com/complete/search?client=firefox&q=${query}`
+      `${PROXY}/https://suggestqueries.google.com/complete/search?client=firefox&q=${query}`
     );
     return result;
   } catch (err) {
