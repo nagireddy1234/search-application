6f76abb96605ab3249edadbb9cc1666221be8b78
nagireddy1234 <nagireddy.panditi@gmail.com>
2021-10-18 09:40:55 +0530

    added the proxy

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const PROXY = "https://cors-anywhere.herokuapp.com";
+const PROXY = "https://thingproxy.freeboard.io/fetch/";
 
 export const searchAPIcall = async (query: string) => {
   try {
     const result = await axios(
-      `${PROXY}/https://suggestqueries.google.com/complete/search?client=firefox&q=${query}`
+      `${PROXY}https://suggestqueries.google.com/complete/search?client=firefox&q=${query}`
     );
     return result;
   } catch (err) {
