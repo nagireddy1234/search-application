54085f762ddd3d9763bbaa8b2395ea9c546a6e89
nagireddy1234 <nagireddy.panditi@gmail.com>
2021-11-21 09:46:03 +0200

    Fixed yarn lock file

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,9 @@
 import axios from "axios";
 
-const PROXY = "https://cors-anywhere.herokuapp.com/";
-
 export const searchAPIcall = async (query: string) => {
   try {
     const result = await axios(
-      `${PROXY}https://suggestqueries.google.com/complete/search?client=firefox&q=${query}`
+      `https://suggestqueries.google.com/complete/search?client=firefox&q=${query}`
     );
     return result;
   } catch (err) {
